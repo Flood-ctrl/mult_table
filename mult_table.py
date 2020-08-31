664f62a9f34218838546fadda04871acc3606475
Flood-ctrl <my@example.com>
2020-08-31 21:51:03 +0300

    counter: add variable

--- mult_table.py
+++ mult_table.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python3
 
 wrong_answers = 0
-re_multiplier = None 
+re_multiplier = None
 re_multiplicable = None
+test_question = int(0)
 
 for multiplicable in range(1, 10):
     for multiplier in range(1, 10):
+        test_question = +1
         if re_multiplicable is not None:
             multiplicable = re_multiplicable
             multiplier = re_multiplier
