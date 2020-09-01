1d43102ec596be959b83484a986b536988edecd6
Flood-ctrl <my@example.com>
2020-09-01 21:46:26 +0300

    tuple: Add tuple and while loop

--- mult_table.py
+++ mult_table.py
@@ -1,31 +1,40 @@
 #!/usr/bin/env python3
 
+import random
+
+elementes = (2, 3, 4, 5, 6, 7, 8)
 wrong_answers = 0
 re_multiplier = None
 re_multiplicable = None
 test_question = int(0)
+attempts = int(5)
 
-for multiplicable in range(1, 10):
-    for multiplier in range(1, 10):
-        test_question = +1
-        if re_multiplicable is not None:
-            multiplicable = re_multiplicable
-            multiplier = re_multiplier
-            re_multiplier = None 
-            re_multiplicable = None
-        result = multiplicable * multiplier
-        print(f"{multiplicable} x {multiplier} =", end=' ')
-        try:
-            input_result = int(input())
-        except ValueError:
-            print("Only numbers allowed")
-            re_multiplicable = multiplicable
-            re_multiplier = multiplier
-            continue
-        if input_result == result:
-            continue
-        else:
-            wrong_answers = +1
-            print(f"{multiplicable} x {multiplier} = {result}")
+while test_question < 5:
+    
+    print(f"attempts {attempts} and question - {test_question}")
+    multiplicable = random.choice(elementes)
+    multiplier = random.choice(elementes)
+
+    if re_multiplicable is not None:
+        multiplicable = re_multiplicable
+        multiplier = re_multiplier
+        re_multiplier = None 
+        re_multiplicable = None
+
+    result = multiplicable * multiplier
+    print(f"{multiplicable} x {multiplier} =", end=' ')
+
+    try:
+        input_result = int(input())
+    except ValueError:
+        print("Only numbers allowed")
+        re_multiplicable = multiplicable
+        re_multiplier = multiplier
+        continue
+
+    if input_result != result:
+        wrong_answers = +1
+        print(f"{multiplicable} x {multiplier} = {result}")
+    test_question += 1
 
 print(f"Wrong answers - {wrong_answers}")
